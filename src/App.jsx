0b35380104604b2623a9d5f3e02385a7a7c43a19
Pawan Kumar <pawan4super30@gmail.com>
2024-09-21 16:52:11 +0530

    application initialized in github repository

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='app-container'>
+      <div className='dashboard-container'>
+        <h1>Hi, dashboard</h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
